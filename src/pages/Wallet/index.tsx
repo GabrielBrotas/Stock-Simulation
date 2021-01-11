7aaedf2b5c2f6eb19468ca0f0d47b18ab4ab1ad3
GabrielBrotas <gabrielbrotas22@gmail.com>
2021-01-11 15:41:50 -0300

    wallet

--- src/pages/Wallet/index.tsx
+++ src/pages/Wallet/index.tsx
@@ -16,6 +16,44 @@ function Wallet() {
                 <div className="app-main-wrapper">
                     <AppHeader title="Carteira" />
 
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Código</td>
+                                <td>Nome</td>
+                                <td>Quantidade</td>
+                                <td>Preço</td>
+                                <td>Total</td>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>TSLA34</td>
+                                <td>Tesla </td>
+                                <td>10</td>
+                                <td>346,00</td>
+                                <td>34600,00</td>
+                            </tr>
+                        </tbody>
+                        
+                        <tfoot>
+                            <tr className="money-left">
+                                <td>
+                                    Dinheiro
+                                </td>
+                                <td>
+                                    R$100.000
+                                </td>
+                            </tr>
+                            
+                            <tr className="total-money">
+                                <td>
+                                    R$100.000
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
                 </div>                
             </main>
         </div>
